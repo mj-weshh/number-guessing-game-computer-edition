eeea0a594e081343b9b9c7f1e20b188e94cf84d4
JOHN WAWERU MUHURA <jowamumjwesh@gmail.com>
2024-02-03 11:05:44 +0300

    Add files via upload

---
+++ guessing_game_user.py
@@ -0,0 +1,20 @@
+import random 
+
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ""
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low
+        feedback = input(f"Is {guess} too high (H), too low (L) or correct\
+ (C)?: ").lower()
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+    print(f"!!!The computer guessed {guess} correctly!!!")
+    
+computer_guess(100)
